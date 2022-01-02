5a4ca7f6a652358e7293f4ce1f52bd27feb2cce5
Jeong Ukjae <JeongUkJae@gmail.com>
2022-01-03 08:51:26 +0900

    add instruction formatter

--- code/code_test.go
+++ code/code_test.go
@@ -9,17 +9,57 @@ import (
 func TestMake(t *testing.T) {
 	tests := []struct {
 		op       Opcode
-		operadns []int
+		operands []int
 		expeced  []byte
 	}{
 		{OpConstant, []int{65534}, []byte{byte(OpConstant), 255, 254}},
 	}
 
 	for _, tt := range tests {
-		instruction, err := Make(tt.op, tt.operadns...)
-		require.Nil(t, err)
+		instruction := Make(tt.op, tt.operands...)
 
 		require.Equal(t, len(tt.expeced), len(instruction))
 		require.ElementsMatch(t, tt.expeced, instruction)
+	}
+}
+
+func TestInstructionString(t *testing.T) {
+	instructions := []Instructions{
+		Make(OpConstant, 1),
+		Make(OpConstant, 2),
+		Make(OpConstant, 65535),
+	}
+
+	expected := `0000 OpConstant 1
+0003 OpConstant 2
+0006 OpConstant 65535
+`
+
+	concatenated := Instructions{}
+	for _, ins := range instructions {
+		concatenated = append(concatenated, ins...)
+	}
+
+	require.Equal(t, expected, concatenated.String(), "instructions wrongly formatted")
+}
+
+func TestReadOperands(t *testing.T) {
+	tests := []struct {
+		op        Opcode
+		operands  []int
+		bytesRead int
+	}{
+		{OpConstant, []int{65535}, 2},
+	}
+
+	for _, tt := range tests {
+		instruction := Make(tt.op, tt.operands...)
+		def, err := Lookup(byte(tt.op))
+		require.Nil(t, err, "definition not found")
+
+		operandsRead, n := ReadOperands(def, instruction[1:])
+		require.Equal(t, tt.bytesRead, n, "wrong #bytesReads")
+
+		require.ElementsMatch(t, tt.operands, operandsRead, "operand wrong")
 	}
 }
