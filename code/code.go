883a526590fcdf879706e006a0c1eeed1788b1f4
Jeong Ukjae <jeongukjae@gmail.com>
2022-01-03 22:28:48 +0900

    add OpAdd

--- code/code.go
+++ code/code.go
@@ -36,6 +36,8 @@ func (ins Instructions) fmtInstruction(def *Definition, operands []int) string {
 	}
 
 	switch operandCount {
+	case 0:
+		return def.Name
 	case 1:
 		return fmt.Sprintf("%s %d", def.Name, operands[0])
 	}
@@ -52,10 +54,12 @@ type Definition struct {
 
 const (
 	OpConstant Opcode = iota
+	OpAdd
 )
 
 var definitions = map[Opcode]*Definition{
 	OpConstant: {"OpConstant", []int{2}},
+	OpAdd:      {"OpAdd", []int{}},
 }
 
 func Lookup(op byte) (*Definition, error) {
--- code/code_test.go
+++ code/code_test.go
@@ -13,6 +13,7 @@ func TestMake(t *testing.T) {
 		expeced  []byte
 	}{
 		{OpConstant, []int{65534}, []byte{byte(OpConstant), 255, 254}},
+		{OpAdd, []int{}, []byte{byte(OpAdd)}},
 	}
 
 	for _, tt := range tests {
@@ -25,14 +26,16 @@ func TestMake(t *testing.T) {
 
 func TestInstructionString(t *testing.T) {
 	instructions := []Instructions{
+		Make(OpAdd),
 		Make(OpConstant, 1),
 		Make(OpConstant, 2),
 		Make(OpConstant, 65535),
 	}
 
-	expected := `0000 OpConstant 1
-0003 OpConstant 2
-0006 OpConstant 65535
+	expected := `0000 OpAdd
+0001 OpConstant 1
+0004 OpConstant 2
+0007 OpConstant 65535
 `
 
 	concatenated := Instructions{}
