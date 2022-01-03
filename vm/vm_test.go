baa645c2514561ec94634389be8e05b0402ad802
Jeong Ukjae <jeongukjae@gmail.com>
2022-01-03 22:15:05 +0900

    add vm base code

---
+++ vm/vm_test.go
@@ -0,0 +1,68 @@
+package vm
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"monkey/ast"
+	"monkey/compiler"
+	"monkey/lexer"
+	"monkey/object"
+	"monkey/parser"
+)
+
+type vmTestCase struct {
+	input    string
+	expected interface{}
+}
+
+func TestIntegerVm(t *testing.T) {
+	tests := []vmTestCase{
+		{"1", 1},
+		{"2", 2},
+		{"1 + 2", 2}, // TODO
+	}
+
+	for _, tt := range tests {
+		runVmTest(t, tt)
+	}
+}
+
+func runVmTest(t *testing.T, tt vmTestCase) {
+	t.Helper()
+
+	program := parse(tt.input)
+
+	comp := compiler.New()
+	err := comp.Compile(program)
+	require.Nil(t, err, "compiler error")
+
+	vm := New(comp.Bytecode())
+	err = vm.Run()
+	require.Nil(t, err, "vm error")
+
+	stackElem := vm.StackTop()
+	testExpectObject(t, tt.expected, stackElem)
+}
+
+func parse(input string) *ast.Program {
+	l := lexer.New(input)
+	p := parser.New(l)
+	return p.ParseProgram()
+}
+
+func testExpectObject(t *testing.T, expected interface{}, actual object.Object) {
+	t.Helper()
+
+	switch expected := expected.(type) {
+	case int:
+		testIntegerObject(t, int64(expected), actual)
+	}
+}
+
+func testIntegerObject(t *testing.T, expected int64, actual object.Object) {
+	result, ok := actual.(*object.Integer)
+	require.True(t, ok)
+	require.Equal(t, expected, result.Value, "object has wrong value")
+}
