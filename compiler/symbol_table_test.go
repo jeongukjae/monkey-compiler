e331efe087d297a00ca68e9680b86bffe2c35807
Jeong Ukjae <jeongukjae@gmail.com>
2022-03-01 13:56:43 +0900

    fixlint

--- compiler/symbol_table_test.go
+++ compiler/symbol_table_test.go
@@ -8,8 +8,8 @@ import (
 
 func TestDefine(t *testing.T) {
 	expected := map[string]Symbol{
-		"a": Symbol{Name: "a", Scope: GlobalScope, Index: 0},
-		"b": Symbol{Name: "b", Scope: GlobalScope, Index: 1},
+		"a": {Name: "a", Scope: GlobalScope, Index: 0},
+		"b": {Name: "b", Scope: GlobalScope, Index: 1},
 	}
 
 	global := NewSymbolTable()
@@ -27,8 +27,8 @@ func TestResolveGlobal(t *testing.T) {
 	global.Define("b")
 
 	expected := map[string]Symbol{
-		"a": Symbol{Name: "a", Scope: GlobalScope, Index: 0},
-		"b": Symbol{Name: "b", Scope: GlobalScope, Index: 1},
+		"a": {Name: "a", Scope: GlobalScope, Index: 0},
+		"b": {Name: "b", Scope: GlobalScope, Index: 1},
 	}
 
 	for _, sym := range expected {
