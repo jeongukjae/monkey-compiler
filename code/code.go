3bf98349a7fe7c1aef07f24c7012ab0725404f93
Jeong Ukjae <jeongukjae@gmail.com>
2022-03-01 11:47:37 +0900

    array op compile

--- code/code.go
+++ code/code.go
@@ -77,6 +77,8 @@ const (
 
 	OpGetGlobal
 	OpSetGlobal
+
+	OpArray
 )
 
 var definitions = map[Opcode]*Definition{
@@ -104,6 +106,8 @@ var definitions = map[Opcode]*Definition{
 
 	OpGetGlobal: {"OpGetGlobal", []int{2}},
 	OpSetGlobal: {"OpSetGlobal", []int{2}},
+
+	OpArray: {"OpArray", []int{2}},
 }
 
 func Lookup(op byte) (*Definition, error) {
