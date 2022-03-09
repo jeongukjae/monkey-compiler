448c5b870aa1f7dfc518db1f34cf6b5d85828637
Ukjae Jeong (Jay) <JeongUkJae@gmail.com>
2022-03-09 07:40:29 +0000

    add readoperands for one byte op

--- code/code.go
+++ code/code.go
@@ -168,6 +168,8 @@ func ReadOperands(def *Definition, ins Instructions) ([]int, int) {
 
 	for i, width := range def.OperandWidths {
 		switch width {
+		case 1:
+			operands[i] = int(ReadUint8(ins[offset:]))
 		case 2:
 			operands[i] = int(ReadUint16(ins[offset:]))
 		}
@@ -180,4 +182,8 @@ func ReadOperands(def *Definition, ins Instructions) ([]int, int) {
 
 func ReadUint16(ins Instructions) uint16 {
 	return binary.BigEndian.Uint16(ins)
+}
+
+func ReadUint8(ins Instructions) uint8 {
+	return uint8(ins[0])
 }
