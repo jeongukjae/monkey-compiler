ff54cc46d1fee9c28b7435597aad203ea00eda98
Ukjae Jeong (Jay) <JeongUkJae@gmail.com>
2022-03-09 15:17:19 +0000

    add closure

--- vm/vm_test.go
+++ vm/vm_test.go
@@ -443,6 +443,139 @@ func TestBuiltinFunctions(t *testing.T) {
 	}
 }
 
+func TestClosures(t *testing.T) {
+	tests := []vmTestCase{
+		{
+			input: `
+			let newClosure = fn(a) {
+				fn() {a;}
+			}
+			let closure = newClosure(99);
+			closure();
+			`,
+			expected: 99,
+		},
+		{
+			input: `
+			let newAdder = fn(a, b) {
+				fn(c) { a+ b + c};
+			}
+			let adder = newAdder(1,2);
+			adder(8)
+			`,
+			expected: 11,
+		},
+		{
+			input: `
+			let newAdder = fn(a, b) {
+				let c = a + b;
+				fn(d) { c+d};
+			}
+			let adder = newAdder(1,2);
+			adder(8)
+			`,
+			expected: 11,
+		},
+		{
+			input: `
+			let newAdderOuter = fn(a, b) {
+				let c = a + b;
+				fn(d) {
+					let e = d + c;
+					fn(f) { e + f; };
+				}
+			}
+			let newAdderInner = newAdderOuter(1, 2);
+			let adder = newAdderInner(3);
+			adder(8);
+			`,
+			expected: 14,
+		},
+		{
+			input: `
+			let a = 1;
+			let newAdderOuter = fn(b) {
+				fn(c) {
+					fn(d) {a + b + c + d};
+				}
+			}
+			let newAdderInner = newAdderOuter(2);
+			let adder = newAdderInner(3);
+			adder(8);
+			`,
+			expected: 14,
+		},
+		{
+			input: `
+			let newClosure = fn(a, b) {
+				let one = fn() {a;}
+				let two = fn() {b;}
+				fn() { one() + two() }
+			}
+			let closure = newClosure(9, 90);
+			closure();
+			`,
+			expected: 99,
+		},
+	}
+
+	for _, tt := range tests {
+		runVmTest(t, tt)
+	}
+}
+
+func TestRecursiveFunctions(t *testing.T) {
+	tests := []vmTestCase{
+		{
+			input: `
+			let countDown = fn(x) {
+				if (x == 0) {
+					return 0;
+				}
+				return countDown(x - 1);
+			}
+			countDown(1);
+			`,
+			expected: 0,
+		},
+		{
+			input: `
+			let countDown = fn(x) {
+				if (x == 0) {
+					return 0;
+				}
+				return countDown(x - 1);
+			}
+			let wrapper = fn() {
+				countDown(1);
+			}
+			wrapper()
+			`,
+			expected: 0,
+		},
+		{
+			input: `
+			let wrapper = fn() {
+				let countDown = fn(x) {
+					if (x == 0) {
+						return 0;
+					}
+					return countDown(x - 1);
+				}
+
+				countDown(1);
+			}
+			wrapper();
+			`,
+			expected: 0,
+		},
+	}
+
+	for _, tt := range tests {
+		runVmTest(t, tt)
+	}
+}
+
 // helper functions
 func runVmTest(t *testing.T, tt vmTestCase) {
 	t.Helper()
