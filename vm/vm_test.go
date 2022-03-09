d6a139e41a624d728fcdb80977c9e5f8b44c9b9e
Ukjae Jeong (Jay) <JeongUkJae@gmail.com>
2022-03-09 15:27:01 +0000

    end

--- vm/vm_test.go
+++ vm/vm_test.go
@@ -576,6 +576,32 @@ func TestRecursiveFunctions(t *testing.T) {
 	}
 }
 
+func TestRecursiveFibonacci(t *testing.T) {
+	tests := []vmTestCase{
+		{
+			input: `
+			let fibonacci = fn(x) {
+				if (x == 0) {
+					return 0;
+				}
+				if (x == 1) {
+					return 1;
+				}
+
+				return fibonacci(x - 1) + fibonacci(x - 2);
+			}
+
+			fibonacci(15);
+			`,
+			expected: 610,
+		},
+	}
+
+	for _, tt := range tests {
+		runVmTest(t, tt)
+	}
+}
+
 // helper functions
 func runVmTest(t *testing.T, tt vmTestCase) {
 	t.Helper()
