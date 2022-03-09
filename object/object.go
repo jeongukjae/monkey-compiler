d80d610468121d71255ae1879c58f0e1fe74fd8d
Ukjae Jeong (Jay) <JeongUkJae@gmail.com>
2022-03-09 08:18:52 +0000

    add local binding in vm

--- object/object.go
+++ object/object.go
@@ -174,6 +174,7 @@ func (h *Hash) Inspect() string {
 
 type CompiledFunction struct {
 	Instructions code.Instructions
+	NumLocals    int
 }
 
 func (cf *CompiledFunction) Type() ObjectType { return COMPILED_FUNCTION_OBJ }
